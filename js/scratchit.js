5ec2c3bada0eae1efd36c07da0692e6c62c02d76
japuser <joaopereira3870@gmail.com>
2020-04-23 20:16:44 +0100

    TouchStart

--- js/scratchit.js
+++ js/scratchit.js
@@ -65,7 +65,8 @@
             this.offsetxy  = this.canvas.offset();
             
             this.canvas.on({
-              'mouseover.scratchit' : $.proxy(this.onDown, this),
+              'mousedown.scratchit' : $.proxy(this.onDown, this),
+              'touchstart.scratchit' : $.proxy(this.onDown, this),
               'mousemove.scratchit' : $.proxy(this.onMove, this),
               'mouseup.scratchit' : $.proxy(this.onUp, this)
             });
