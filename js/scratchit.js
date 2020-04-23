76a62c7682051e9a30bbd39f0780b2a567288151
japuser <joaopereira3870@gmail.com>
2020-04-23 20:25:02 +0100

    ASas

--- js/scratchit.js
+++ js/scratchit.js
@@ -65,9 +65,12 @@
             this.offsetxy  = this.canvas.offset();
             
             this.canvas.on({
+              'mousedown.scratchit' : $.proxy(this.onDown, this),
               'touchstart.scratchit' : $.proxy(this.onDown, this),
               'mousemove.scratchit' : $.proxy(this.onMove, this),
-              'mouseup.scratchit' : $.proxy(this.onUp, this)
+              'touchmove.scratchit' : $.proxy(this.onMove, this),
+              'mouseup.scratchit' : $.proxy(this.onUp, this),
+              'touchend.scratchit' : $.proxy(this.onUp, this)
             });
         },
         
