83b9ee0f37d3bca53119ce32607fe1bd0134219e
japuser <joaopereira3870@gmail.com>
2020-08-20 11:57:34 +0100

    asd

--- js/scratchit.js
+++ js/scratchit.js
@@ -5,8 +5,8 @@
   var isDrawing, lastPoint;
   var container    = document.getElementById('js-container'),
       canvas       = document.getElementById('js-canvas'),
-      canvasWidth  = 300,
-      canvasHeight = 300,
+      canvasWidth  = canvas.300,
+      canvasHeight = canvas.height,
       ctx          = canvas.getContext('2d'),
       image        = new Image(),
       brush        = new Image();
